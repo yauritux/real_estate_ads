833652b88598f9ea8b71d88a1f9d2ccd4fe5cf7b
Yauri Attamimi <yauritux@gmail.com>
2024-06-22 15:51:39 +0700

    feat: implement scheduler action for extending offer deadline

--- real_estate_ads/models/property_offer.py
+++ real_estate_ads/models/property_offer.py
@@ -123,3 +123,8 @@ class PropertyOffer(models.Model):
             offer_ids = self.env['estate.property.offer'].browse(active_ids)
             for offer in offer_ids:
                 offer.validity = 10
+
+    def _extend_offer_deadline(self):
+        offer_ids = self.env['estate.property.offer'].search([])
+        for offer in offer_ids:
+            offer.validity = offer.validity + 1
